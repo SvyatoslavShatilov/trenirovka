6663876b412cb3646d5b1db9101fb94b9ccc1e78
SvyatoslavShatilov <160323879+SvyatoslavShatilov@users.noreply.github.com>
2024-06-01 19:16:14 +0000

    учимся делать ввод чисел в программу для пользователя

--- .ccls-cache/@home@runner@trenirovka/main.cpp
+++ .ccls-cache/@home@runner@trenirovka/main.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-
+/*
 int main() {
   int a;
   int b;
@@ -7,4 +7,24 @@ int main() {
   std::cin >> a;
   b = a * a;
   std::cout << "Вы ввели число и получили результат: " << b << "\n";
+}*/
+
+int main() {
+ int productCost;
+ int deliveryCost;
+ int discount;
+  std::cout << "Введите стоимость товара: ";
+  std::cin >> productCost;
+  std::cout << "Введите стоимость доставки: ";
+  std::cin >> deliveryCost;
+  std::cout << "Введите размер скидки: ";
+  std::cin >> discount;
+  
+ int price = productCost + deliveryCost - discount;
+
+ std::cout << "Стоимость товара: " << productCost << "\n";
+ std::cout << "Cтоимость доставки: " << deliveryCost << "\n";
+ std::cout << "Скидка: " << discount << "\n";
+ std::cout << "---------\n";
+ std::cout << "Итого: " << price << "\n";
 }
